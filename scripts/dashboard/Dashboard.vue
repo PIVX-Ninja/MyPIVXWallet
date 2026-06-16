3262b99e12d2457228b00ed8522574e225ce09ef
alexxiy <alexey.savik@gmail.com>
2026-06-16 14:21:55 +0300

    Fixes for our testnet

--- scripts/dashboard/Dashboard.vue
+++ scripts/dashboard/Dashboard.vue
@@ -113,7 +113,7 @@ async function importWallet({
     secret,
     password = '',
     label,
-    blockCount = 4_200_000,
+    blockCount = cChainParams.current.name === 'mainnet' ? 4_200_000 : 201,
 }) {
     try {
         /**
